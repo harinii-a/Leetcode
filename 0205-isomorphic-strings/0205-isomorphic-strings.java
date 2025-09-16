653842db84bf1f6caccdc5a790b8eec9181f2aa0
Harini A <harini.vaidy16@gmail.com>
2025-09-16 21:33:53 +0530

    Time: 17 ms (35.1%), Space: 42.8 MB (14.44%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,31 +1,3 @@
-// class Solution {
-//     public boolean isIsomorphic(String s, String t) {
-//         if(s.length()!=t.length()) return false;
-//         HashMap<Character,Character> map = new HashMap<>();
-//         for(int i=0;i<t.length();i++){
-//             if(!(map.containsKey(t.charAt(i)))){
-//                 if(!(map.containsValue(s.charAt(i)))){ 
-//                     map.put(t.charAt(i),s.charAt(i));
-//                 }
-//             }
-//             if(map.containsKey(t.charAt(i))){
-//                 if(map.get(t.charAt(i)) != s.charAt(i)){
-//                     return false;
-//                 }
-//             }
-//             if(map.containsValue(s.charAt(i))){
-//                 if(map.get(s.charAt(i)) != t.charAt(i)){
-//                     return false;
-//                 }
-//             }
-
-
-//         }
-
-//         return true;
-//     }
-// }
-
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         if(s.length()!=t.length()) return false;
@@ -42,7 +14,6 @@ class Solution {
             if(!map.containsKey(a)){
                 if(map.containsValue(b)) return false;
             }
-
             //key ila value iruku
             map.put(a,b);
 
