7838862f1f6314206f96c04ed7d07f7c7efb92cb
Harini A <harini.vaidy16@gmail.com>
2026-03-14 22:47:54 +0530

    Time: 0 ms (100%), Space: 44.1 MB (20.04%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,33 +1,60 @@
-class Solution {
+// class Solution {
 
-    public static int binarySearch(int left , int right, int target, int arr[][],int n){
+//     public static int binarySearch(int left , int right, int target, int arr[][],int n){
+//         while(left<=right){
+//             int mid = (left+right) / 2;
+//             if(arr[n][mid]==target) return mid;
+//             if(arr[n][mid]>target){
+//                 right = mid-1;
+//             }
+//             else{
+//                 left = mid+1;
+//             }
+//         }
+//         return -1;
+//     }
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         boolean res = false;
+//         int len = matrix.length;
+//         int innerlen = matrix[0].length -1;
+//         int index = -1;
+//         for(int i=0;i<len;i++){
+//             if(matrix[i][innerlen] >= target){
+//                 index = i;
+//                 break;
+//             }
+//         }
+//         if(index == -1) return res;
+//         int num = binarySearch(0,innerlen,target,matrix,index);
+//         if(num == -1) return res;
+//         else return true;
+        
+//     }
+// }
+
+
+class Solution{
+        public static boolean binarySearch(int left , int target, int arr[][]){
+            int n = arr[0].length;
+            int right = (arr.length * n)-1;
         while(left<=right){
-            int mid = (left+right) / 2;
-            if(arr[n][mid]==target) return mid;
-            if(arr[n][mid]>target){
-                right = mid-1;
+            int mid = (left + right) / 2;
+            int row = mid / n;
+            int col = mid % n;
+            if(arr[row][col]==target) return true;
+            if(arr[row][col]>target){
+                right = mid -1;
             }
             else{
                 left = mid+1;
             }
         }
-        return -1;
+        return false;
     }
+
+
     public boolean searchMatrix(int[][] matrix, int target) {
-        boolean res = false;
-        int len = matrix.length;
-        int innerlen = matrix[0].length -1;
-        int index = -1;
-        for(int i=0;i<len;i++){
-            if(matrix[i][innerlen] >= target){
-                index = i;
-                break;
-            }
-        }
-        if(index == -1) return res;
-        int num = binarySearch(0,innerlen,target,matrix,index);
-        if(num == -1) return res;
-        else return true;
-        
+        return binarySearch(0,target,matrix);
+
     }
 }
